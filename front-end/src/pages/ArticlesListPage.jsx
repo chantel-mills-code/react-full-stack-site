b12aa0c3e3ade3641fc94aa0d189f765729f82fa
chantel-mills <clmsyd2001@gmail.com>
2025-09-03 16:03:34 +1000

    create and link articles/article lists

--- front-end/src/pages/ArticlesListPage.jsx
+++ front-end/src/pages/ArticlesListPage.jsx
@@ -1,5 +1,12 @@
+import ArticlesList from "../ArticlesList";
+import articles from "../article-content";
+import { Link } from "react-router-dom";
+
 export default function ArticleListPage() {
     return (
-        <h1>This is the article list page</h1>
+        <>
+            <h1>Articles</h1>
+            <ArticlesList articles={articles} />
+        </>
     )
 }
