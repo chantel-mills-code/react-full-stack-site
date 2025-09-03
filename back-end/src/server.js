2ac055a0800e4b3142244918e3a9b0913f12c2ea
chantel-mills <clmsyd2001@gmail.com>
2025-09-03 17:33:23 +1000

    test server endpoints - get + post

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,10 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json()); // if app sees a request, it should add it to request.body
+
 app.get('/hello', function(req, res) {
-    res.send('Hello!');
+    res.send('Hello from a GET endpoint!');
+});
+
+app.post('/hello', function(req, res) {
+    res.send('Hello, ' + req.body.name + ' from a POST endpoint')
 });
 
 app.listen(8000, function() {
     console.log("Server is listening on port 8000");
-})
+});
