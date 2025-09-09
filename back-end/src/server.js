25a69007049c25b62014060e2a2a7f0f8136f2c4
chantel-mills <clmsyd2001@gmail.com>
2025-09-09 20:59:55 +1000

    rewrite upvote endpoint - connect to mongodb db

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ReturnDocument, ServerApiVersion } from 'mongodb';
 
 // dummy db
 const article_info = [
@@ -12,10 +12,9 @@ const app = express();
 
 app.use(express.json()); // if app sees a request, it should add it to request.body
 
-// will be able to load data for a specific article
-app.get('/api/articles/:name', async (req, res) => {
-    const { name } = req.params;
-    
+let db;
+
+async function connectToDB() {
     // connect to mongodb
     const uri = 'mongodb://127.0.0.1:27017';
 
@@ -29,19 +28,28 @@ app.get('/api/articles/:name', async (req, res) => {
 
     await client.connect();
 
-    const db = client.db('full-stack-react-db');
+    db = client.db('full-stack-react-db');
+}
+
+// will be able to load data for a specific article
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
 
     const article = await db.collection('articles').findOne({name});
 
     res.json(article);
 });
 
-app.post('/api/articles/:name/upvote', function(req, res) {
-    const article = article_info.find(a => a.name === req.params.name);
-    article.upvotes += 1;
+app.post('/api/articles/:name/upvote', async function(req, res) {
+    const { name } = req.params;
 
-    // res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!');
-    res.json(article);
+    const updatedArticle = await db.collection('articles').findOneAndUpdate({ name }, {
+        $inc:  { upvotes: 1 } // $inc ..=> increments the property in table by designated amount to increment by
+    }, {
+        returnDocument: "after",
+    });
+
+    res.json(updatedArticle);
 });
 
 app.post('/api/articles/:name/comments', (req, res) => {
@@ -58,19 +66,12 @@ app.post('/api/articles/:name/comments', (req, res) => {
     res.json(article); // sends back the updated article as a response
 });
 
-////// examples for routing in express server
-// app.get('/hello', function(req, res) {
-//     res.send('Hello from a GET endpoint!');
-// });
+async function start() {
+    await connectToDB();
+    
+    app.listen(8000, function() {
+        console.log("Server is listening on port 8000");
+    });
+}
 
-// app.get('/hello/:name', function(req, res) {
-//     res.send('Hello, ' + req.params.name);
-// })
-
-// app.post('/hello', function(req, res) {
-//     res.send('Hello, ' + req.body.name + ' from a POST endpoint')
-// });
-
-app.listen(8000, function() {
-    console.log("Server is listening on port 8000");
-});
+start();
