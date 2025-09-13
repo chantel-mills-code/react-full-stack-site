6cc7daa2d7f8d705d0a2e7f02b7c29b3d35d23b0
chantel-mills <clmsyd2001@gmail.com>
2025-09-13 12:09:22 +1000

    add way to view comments on article page

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,6 +1,7 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import articles from '../article-content';
 import axios from 'axios';
+import CommentsList from "../CommentsList";
 
 export default function ArticlePage() {
     const params = useParams();
@@ -18,6 +19,7 @@ export default function ArticlePage() {
             <h1>{article.title}</h1>
             <p>This article has {upvotes} upvotes!</p>
             {article.content.map(p => <p key={p}>{p}</p>)}
+            <CommentsList comments={comments}/>
         </>
     )
 }
