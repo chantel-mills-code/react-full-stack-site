44ebc1adfdaf4e6f56648a46affb1a6456579f58
chantel-mills <clmsyd2001@gmail.com>
2025-09-21 21:59:38 +1000

    adjust UI when user logged in or not
    temporarily hard coded

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function NavBar() {
+    const isLoggedIn = false;
+    const email = "ellePlaceholder@gmail.com"
+
+    const navigate = useNavigate();
+
     return (
         <nav>
             <ul>
@@ -12,6 +19,17 @@ export default function NavBar() {
                 </li>
                 <li>
                     <Link to='/articles'>Articles</Link>
+                </li>
+                {isLoggedIn && (
+                    <li style={{color: 'white'}}>
+                        Logged in as: {email}
+                    </li>
+                )}
+                <li>
+                    {isLoggedIn 
+                        ? <button onClick={() => signOut(getAuth())}>Log Out</button>
+                        : <button onClick={() => navigate('/login')}>Log In</button>
+                    }
                 </li>
             </ul>
         </nav>
